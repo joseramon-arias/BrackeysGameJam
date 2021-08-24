58a757defac5896129f1d8406264a15fcfba206b
Jose Ramon Arias <joseramon.arias28@gmail.com>
2021-08-23 22:24:17 -0700

    Added player rotation towards mouse

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -18,6 +18,19 @@ public class PlayerMovement : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");   
 
-        transform.position += new Vector3(-vertical, 0f, horizontal) * speed * Time.deltaTime;
+        transform.position += new Vector3(horizontal, 0f, vertical) * speed * Time.deltaTime;
+
+        Ray mouseRay = GameObject.Find("Main Camera").GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        Plane p = new Plane(Vector3.up, transform.position);
+        if (p.Raycast(mouseRay, out float hitDist))
+        {
+            Vector3 hitPoint = mouseRay.GetPoint(hitDist);
+            transform.LookAt(hitPoint);
+        }
+    }
+
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
     }
 }
