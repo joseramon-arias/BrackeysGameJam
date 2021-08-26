1962ec2c39507efbe9a5a3ea8badb2e6a3d4f80c
Danny <thedannyvegas@gmail.com>
2021-08-25 22:17:53 -0300

    Basic setup of the weapon module system

---
+++ Assets/WeaponSystem.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSystem : MonoBehaviour
+{
+    public WeaponModule[] modules;
+    private void Start()
+    {
+        List<WeaponModule> weaponModules = new List<WeaponModule>();
+
+
+    }
+}
--- Assets/weaponModule.cs
+++ Assets/weaponModule.cs
@@ -1,8 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class weaponModule : MonoBehaviour
+public enum Type
 {
-    [SerializeField] private GameObject slot;
+    damage,
+    support,
+    mechanical
+}
+
+[CreateAssetMenu(fileName = "Module", menuName = "Weapon Module", order = 0)]
+public class WeaponModule : ScriptableObject
+{
+    public GameObject prefab;
+    public Type type;
+    public string name = "module name";
+    [Multiline]
+    public string description = "sample description";
+    public Sprite icon;
+
+
+    /// <summary>
+    /// Declares a new Weapon Module
+    /// </summary>
+    /// <param name="moduleDistance">Distance from the player (in modules)</param>
+    public WeaponModule()
+    {
+
+    }
 }
